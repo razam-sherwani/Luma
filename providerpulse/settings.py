0e416fb40be154a454ae2b12e021b5791bacc079
CarrotNinja <r-sherwani@outlook.com>
2025-09-28 03:39:14 -0400

    Fix: Add Render domain to ALLOWED_HOSTS for deployment

--- providerpulse/settings.py
+++ providerpulse/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-yv77p_mhixz%w5d3hpla3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,luma-444u.onrender.com,*.onrender.com', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
